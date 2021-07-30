5637ad9bf67eb48eaf420437215e89014d40d3bb
ybc <byeongchan.yoo@deliveryhero.co.kr>
2021-07-30 11:41:58 +0900

    PROL-488-create-post-change-to-jsonresponse

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,100 @@
 from django.test import TestCase
+from .models import Post, Comment
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+from http import HTTPStatus
+import json
 
-# Create your tests here.
+
+class TestPost(TestCase):
+    def _create_new_post(self, user, title, text):
+        post = Post.objects.create(
+            author=self.user, title=title, text=text, published_date=timezone.now()
+        )
+        return post
+
+    def setUp(self):
+        self.username = "Testuser"
+        self.password = "Test123"
+        self.user = User.objects.create(username=self.username)
+        self.user.set_password(self.password)
+        self.user.save()
+
+    def test_get_all_posts(self):
+        # Given : 새로운 Post 생성
+        for _ in range(30):
+            self._create_new_post(self.user, "title", "text")
+
+        # When : Post 모두 조회
+        response = self.client.get(reverse("post_list"))
+
+        # Then : Post들이 잘 조회되는지 확인
+        data = json.loads(response.json()["post_data"])
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(len(data), 30)
+
+    def test_get_one_post(self):
+        # Given : get할 새로운 post 미리 생성
+        post = self._create_new_post(self.user, "test_title", "test_text")
+
+        # When : 생성한 post 조회
+        response = self.client.get(reverse("post_detail", kwargs={"pk": post.pk}))
+
+        # Then : 생성한 post 와 조회한 post를 비교
+        post_data = json.loads(response.json()["post_data"])[0]["fields"]
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(post_data["author"], self.user.pk)
+        self.assertEqual(post_data["title"], post.title)
+        self.assertEqual(post_data["text"], post.text)
+
+    def test_get_not_exist_post(self):
+        # Given : 존재하지 않는 post pk
+        sample_pk = 1234
+
+        # When : 존재하지 않는 post 조회
+        response = self.client.get(reverse("post_detail", kwargs={"pk": sample_pk}))
+
+        # The : NOTFOUND 반환
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+    def test_create_new_post(self):
+        # Given : 로그인 하고 난 후에 post 데이터 생성
+        self.client.login(username=self.username, password=self.password)
+        data = {"title": "test_title", "text": "test_text"}
+
+        # When : post 생성 요청
+        response = self.client.post(reverse("post_new"), data=data)
+
+        # Then : 제대로 생성되었는지 확인
+        post_data = json.loads(response.json()["post_data"])[0]
+        post = post_data["fields"]
+
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertEqual(post["author"], self.user.pk)
+        self.assertEqual(post["title"], data["title"])
+        self.assertEqual(post["text"], data["text"])
+        self.assertIsNone(post["published_date"])
+
+    def test_redirect_login_page_if_not_login(self):
+        # Given : 로그인 안하고 post 데이터 생성
+        data = {"title": "login_page_test", "text": "login_page_test_text"}
+
+        # When : post 생성 요청
+        response = self.client.post(reverse("post_new"), data=data)
+
+        # Then : 제대로 redirect 되었는지 확인
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+    def test_create_bad_post(self):
+        # Given : 로그인 + 잘못된 형식의 데이터
+        self.client.login(username=self.username, password=self.password)
+        data = {"title": "there is no TEXT"}
+
+        # When : post 생성 요청
+        response = self.client.post(reverse("post_new"), data=data)
+
+        # Then : Post 생성되지 않고, Badrequest 반환
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,22 @@ from .models import Post, Comment
 from .form import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from http import HTTPStatus
+from django.http import JsonResponse
+from django.core import serializers
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    post_data = serializers.serialize('json', posts)
+    return JsonResponse({'post_data': post_data}, status=HTTPStatus.OK)
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    post_data = serializers.serialize('json', [post, ])
+    return JsonResponse({'post_data': post_data}, status=HTTPStatus.OK)
+
 
 @login_required
 def post_new(request):
@@ -21,16 +29,21 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_list')
+            post_data = serializers.serialize('json', [post, ])
+            return JsonResponse({'post_data': post_data}, status=HTTPStatus.CREATED)
+        else:
+            msg = {"message": "잘못된 입력입니다."}
+            return JsonResponse(msg, status=HTTPStatus.BAD_REQUEST)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html',{'form' : form})
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 
 @login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance = post)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -38,17 +51,20 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form' : form})
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 
 @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts' : posts})
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
 
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
+
 
 @login_required
 def post_remove(request, pk):
@@ -70,11 +86,13 @@ def add_comment_to_post(request, pk):
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
+
 @login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
+
 
 @login_required
 def comment_remove(request, pk):
