de57307cb4e8c18c6f72c047b07d6f6389ae3cba
Taehyoung.Kwon <taehyoung.kwon@deliveryhero.co.kr>
2021-08-02 17:19:45 +0900

    PROL-510 delete useless view functions

--- blog/views.py
+++ blog/views.py
@@ -14,22 +14,6 @@ from django.forms import model_to_dict
 import json
 
 
-def date_converter(data):
-    if isinstance(data, datetime.datetime):
-        return data.__str__()
-
-
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    post_data = json.dumps([model_to_dict(post) for post in posts], default=date_converter)
-    return JsonResponse({"post_data": post_data}, status=HTTPStatus.OK)
-
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return JsonResponse({"post_data": model_to_dict(post)}, status=HTTPStatus.OK)
-
-
 @login_required
 def post_new(request):
     if request.method == "POST":
@@ -62,50 +46,3 @@ def post_edit(request, pk):
     else:
         post.save()
     return JsonResponse(data={}, status=HTTPStatus.OK)
-
-
-@login_required
-def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts': posts})
-
-
-def post_publish(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.publish()
-    return redirect('post_detail', pk=pk)
-
-
-@login_required
-def post_remove(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.delete()
-    return redirect('post_list')
-
-
-def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
-
-
-@login_required
-def comment_approve(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
-
-
-@login_required
-def comment_remove(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.delete()
-    return redirect('post_detail', pk=comment.post.pk)
